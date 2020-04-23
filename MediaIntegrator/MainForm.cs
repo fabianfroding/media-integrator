066e0d69b8c13afb2910194ade93ca6086f77b67
fabianfroding <fabian.froding@gmail.com>
2020-04-23 16:19:18 +0200

    Implemented file watcher

--- MediaIntegrator/MainForm.cs
+++ MediaIntegrator/MainForm.cs
@@ -4,9 +4,6 @@ namespace media_integrator
 {
     public partial class MainForm : Form
     {
-        private static readonly string FROM_SHOP_PATH = @"..\Debug\frShop\";
-        private static readonly string TO_SIMPLE_PATH = @"..\Debug\toSimple\";
-
         private Parser parser;
 
         public MainForm()
@@ -18,10 +15,8 @@ namespace media_integrator
         //=============== UI Interactives ===============//
         private void BTNIntegrate_Click(object sender, System.EventArgs e)
         {
-            parser.ParseCSVInDir(FROM_SHOP_PATH, TO_SIMPLE_PATH);
+            parser.StartFileWatcher();
         }
-
-
 
     }
 }
--- MediaIntegrator/Parser.cs
+++ MediaIntegrator/Parser.cs
@@ -8,58 +8,82 @@ namespace media_integrator
 {
     class Parser
     {
+        // Paths till de övervakade mapparna och destinations-mapparna.
+        private static readonly string FROM_SIMPLE_PATH = @"..\Debug\frSimple\";
+        private static readonly string FROM_SHOP_PATH = @"..\Debug\frShop\";
+        private static readonly string TO_SIMPLE_PATH = @"..\Debug\toSimple\";
+        private static readonly string TO_SHOP_PATH = @"..\Debug\toShop\";
+        
+        // Product-klassens fält för xml-tags.
+        private static readonly string[] PRODUCT_FIELDS = new string[5]
+        {
+            "Id", "Name", "Price", "Stock", "ProductType"
+        };
 
         //=============== Public Functions ===============//
+        public void StartFileWatcher()
+        {
+            FileSystemWatcher fsw = new FileSystemWatcher(@"..\Debug\frShop\", "*.txt");
+            fsw.Created += new FileSystemEventHandler(FileChanged);
+            fsw.Changed += new FileSystemEventHandler(FileChanged);
+            fsw.Deleted += new FileSystemEventHandler(FileDeleted);
+            fsw.Renamed += new RenamedEventHandler(FileRenamed);
+            fsw.EnableRaisingEvents = true;
+        }
+
         public void ParseCSVInDir(string targetDirPath, string destinationDirPath)
         {
             DirectoryInfo di = new DirectoryInfo(targetDirPath);
-
             FileInfo[] files = di.GetFiles();
             foreach (FileInfo fi in files)
             {
-                //System.Diagnostics.Debug.WriteLine(fi.Name);
-                List<string> lines = ParseCSVFile(fi);
-                ConvertToXML(lines, destinationDirPath);
+                ConvertToXML(fi, destinationDirPath);
             }
         }
 
         //=============== Private Functions ===============//
-        private List<string> ParseCSVFile(FileInfo fi)
+        private void ConvertToXML(FileInfo fi, string destinationDirPath)
         {
             List<string> lines = File.ReadAllLines(fi.FullName).ToList();
-
-            
-
-            return lines;
-        }
-
-        private void ConvertToXML(List<string> lines, string destinationDirPath)
-        {
             XmlWriterSettings settings = new XmlWriterSettings();
             settings.Indent = true;
-            XmlWriter xw = XmlWriter.Create("products.xml", settings);
+            XmlWriter xw = XmlWriter.Create(destinationDirPath, settings);
             
             xw.WriteStartDocument();
             xw.WriteStartElement("Products");
             foreach (string line in lines)
             {
-                
                 if (line != "")
                 {
-                    System.Diagnostics.Debug.WriteLine(line);
+                    xw.WriteStartElement("Product");
                     string[] entries = line.Split('|');
+
                     for (int i = 0; i < entries.Length; i++)
                     {
-                        System.Diagnostics.Debug.WriteLine(entries[i]);
-                        xw.WriteStartElement(entries[1]);
+                        xw.WriteStartElement(PRODUCT_FIELDS[i]);
+                        xw.WriteValue(entries[i]);
                         xw.WriteEndElement();
-
-
                     }
+                    xw.WriteEndElement();
                 }
             }
             xw.WriteEndElement();
             xw.Close();
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.Name + " created or changed.");
+        }
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.Name + " deleted.");
+        }
+
+        private void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.OldName + " renamed to " + e.Name);
         }
     }
 }
